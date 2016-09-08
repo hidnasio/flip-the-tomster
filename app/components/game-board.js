777297fa6ca76ecb70c7b28454a1793ee88caddb
Luis Ferreira <hidnasio@gmail.com>
2016-09-08 16:25:37 -0300

    Improves css

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -4,6 +4,17 @@ export default Ember.Component.extend({
   tagName: '',
   game: Ember.inject.service(),
   isNotEmpty: Ember.computed.notEmpty('setup.cards'),
+  gridSize: Ember.computed('setup.cards', function() {
+    let size = 1;
+
+    if(this.get('setup.cards').length === 16) {
+      size = 3;
+    } else if(this.get('setup.cards').length === 36) {
+      size = 2;
+    }
+
+    return size;
+  }),
   actions: {
     flip(card) {
       this.get('game').flip(card);
