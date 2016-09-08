671d867ffbb3548ffaa70e92969287e7616f16cf
Luis Ferreira <hidnasio@gmail.com>
2016-09-08 11:13:03 -0300

    Adds a lot of color

--- config/environment.js
+++ config/environment.js
@@ -16,7 +16,12 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
+
+    googleFonts: [
+      'Lobster',
+      'Cabin'
+    ]
   };
 
   if (environment === 'development') {
