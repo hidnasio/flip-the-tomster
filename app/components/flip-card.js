00334fe86247b6e18519a7528b228965a600c85c
Luis Ferreira <hidnasio@gmail.com>
2016-09-12 20:26:56 -0300

    Improves responsive behavior

--- app/components/flip-card.js
+++ app/components/flip-card.js
@@ -11,7 +11,7 @@ export default Ember.Component.extend({
     let figure = this.get('card.figure');
     let value = this.get('card.value');
 
-    return `image-${figure}${value}`;
+    return `figure-${figure}${value}`;
   }),
 
   click() {
